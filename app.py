708b151b9d0acfb026e6f9c02f785fe2b94ecdaf
meplenier <mathieu.eplenier@gmail.com>
2025-09-26 17:59:00 +0200

    add command to get projects & jobs

--- app.py
+++ app.py
@@ -16,3 +16,32 @@ def get_dataiku_client():
     client = dataikuapi.DSSClient(HOST, API_KEY, insecure_tls=True)
     return client
 
+
+@st.cache_data
+def get_projets():
+    projects = []
+    for project_key in client.list_project_keys():
+        if project_key.startswith("SIA") or project_key.startswith("SPIDER") or project_key.startswith("GROUP_DATA_SHARING"):
+            projects.append(project_key)
+    return projects
+
+            
+@st.cache_data
+def print_jobs(jobs):
+    for job in jobs:
+        st.write(f"- {job["def"]["name"]}")
+
+
+@st.fragment(run_every=2)
+def get_jobs(project):
+    jobs = [job for job in project.list_jobs() if not job["stableState"]]
+    print_jobs(jobs)
+
+client = get_dataiku_client()
+projects = get_projets()
+
+for project_key in projects:
+    project = client.get_project(project_key)
+    st.write(f"#### {project_key}")
+    get_jobs(project)
+    
